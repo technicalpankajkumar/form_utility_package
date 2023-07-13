eed102ac81fcd50303f1c8e4517be43b2869f438
pankajkumar <pankajkumar@unicodesystems.info>
2023-07-13 18:38:15 +0530

    add git repo for this package

--- dist/form_utility/fields/Select.js
+++ dist/form_utility/fields/Select.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _react = _interopRequireWildcard(require("react"));
 var _excluded = ["options", "onChange", "outerClass", "name", "label"],
-  _excluded2 = ["value", "key", "selected"];
+  _excluded2 = ["value", "key"];
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -30,14 +30,11 @@ var _default = /*#__PURE__*/(0, _react.memo)(function Select(_ref) {
   }, rest), options.map(function (_ref2) {
     var value = _ref2.value,
       key = _ref2.key,
-      _ref2$selected = _ref2.selected,
-      selected = _ref2$selected === void 0 ? false : _ref2$selected,
       rest = _objectWithoutProperties(_ref2, _excluded2);
     return /*#__PURE__*/_react["default"].createElement("option", _extends({
       name: value,
       value: value,
-      key: value,
-      selected: selected
+      key: value
     }, rest), key);
   })));
 });
